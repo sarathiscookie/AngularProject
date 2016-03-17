6526eb988396cd2d302885704c2b13ca6c782bca
Sarath <sarathiscookie@hotmail.com>
2016-03-17 21:34:36 +0530

    Task7: Scope as Data - Model

--- web-DG-scopes-1.php
+++ web-DG-scopes-1.php
@@ -1,7 +1,28 @@
-<?php
-/**
- * Created by PhpStorm.
- * User: php
- * Date: 3/17/2016
- * Time: 9:13 PM
- */
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Example</title>
+</head>
+<body ng-app="appModule">
+<div ng-controller="appController">
+    <div>
+        Name: <input type="text" name="name" ng-model="name">
+    </div>
+    <div>
+        <button ng-click="message();">Click</button>
+    </div>
+    <p>{{ greetings }}</p>
+</div>
+<script src="//ajax.googleapis.com/ajax/libs/angularjs/1.5.0/angular.min.js"></script>
+<script>
+    angular.module('appModule', [])
+        .controller('appController', ['$scope', function($scope){
+            $scope.name = 'sarath ts';
+            $scope.message = function(){
+                $scope.greetings = 'Welcome ' +$scope.name;
+            }
+        }]);
+</script>
+</body>
+</html>
